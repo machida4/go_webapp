fb5aac117b07c3e8283eb72bbd6f0f9d862faade
machida <machida@kmc.gr.jp>
2021-04-13 20:10:39 +0900

    テンプレートをレンダリングして出力するように

--- chat/main.go
+++ chat/main.go
@@ -24,7 +24,7 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-  http.HandleFunc("/", ServeHTTP)
+  http.Handle("/", &templateHandler{filename: "chat.html"})
 
   if err := http.ListenAndServe(":8080", nil); err != nil {
     log.Fatal("ListenAndServe:", err)
